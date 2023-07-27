f3fe378116eac4b00ab59ceb7e4a8cafb163728d
appuser <appuser@journey2-86bff4fbf7-5gdcq>
2023-07-27 06:41:52 +0000

    starter project

---
+++ Codecool.DownTheRabbitHoleRepublic/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Codecool.DownTheRabbitHoleRepublic;
+
+class Program
+{
+    private static readonly string WorkDir = AppDomain.CurrentDomain.BaseDirectory;
+
+    public static void Main(string[] args)
+    {
+        SplitRepublic();
+        SplitAlice();
+
+        Console.ReadKey();
+    }
+
+    private static void SplitRepublic()
+    {
+    }
+
+    private static void SplitAlice()
+    {
+    }
+
+}
