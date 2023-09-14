bd964571591c85250fd5046d5a1ceed90516cb1c
cojocaru-dan <dan.cojocaru90233@gmail.com>
2023-09-14 15:27:55 +0300

    Read 2 books and split them into chapters

--- Codecool.DownTheRabbitHoleRepublic/Program.cs
+++ Codecool.DownTheRabbitHoleRepublic/Program.cs
@@ -14,10 +14,52 @@ class Program
 
     private static void SplitRepublic()
     {
+        using var reader = new StreamReader("Resources//the-republic.txt");
+        string path = "the-republic//";
+        try
+        {
+            string fileText = reader.ReadToEnd();
+            string[] chapters = fileText.Split("# CHAPTER", StringSplitOptions.RemoveEmptyEntries)[1..];
+
+            foreach (var chapter in chapters)
+            {
+                string chapterName = chapter[..chapter.IndexOf(".")];
+                string chapterFileName = path + $"the-republic - {chapterName}.txt";
+
+                using StreamWriter writer = new StreamWriter(chapterFileName);
+                writer.Write(chapter);
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 
     private static void SplitAlice()
     {
+        using var reader = new StreamReader("Resources\\alice-in-wonderland.txt");
+        try
+        {
+            string fileText = reader.ReadToEnd();
+            string[] chapters = fileText.Split("#chapter", StringSplitOptions.RemoveEmptyEntries)[1..];
+
+            string outputDirectory = "alice-in-wonderland";
+            Directory.CreateDirectory(outputDirectory);
+
+            foreach (var chapter in chapters)
+            {
+                string chapterName = chapter[..chapter.IndexOf(".")];
+                string chapterFileName = Path.Combine(outputDirectory, $"alice-in-wonderland - {chapterName}.txt");
+
+                using StreamWriter writer = new StreamWriter(chapterFileName);
+                writer.Write(chapter);
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # down-the-rabbit-hole-republic
+
+## For this project we need to write a program that splits the books The Republic and Alice In Wonderland into chapters and write each chapter into separate file.
+
+### In this project all the code is in the Program.cs file
+
+### What are you going to learn?
+### •	Use file input and output.
+### •	Use string operations.
